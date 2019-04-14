3814158f0efd421735d28b554a309577e157d9bf
Xiangyang Zhi <zhixy@shanghaitech.edu.cn>
2019-04-14 16:15:25 +0800

    simplify algorithm

--- axxb/conventionalaxxbsvdsolver.cc
+++ axxb/conventionalaxxbsvdsolver.cc
@@ -9,7 +9,8 @@
 
 Pose ConventionalAXXBSVDSolver::SolveX()
 {
-  Eigen::MatrixXd m = Eigen::MatrixXd::Zero(12*A_.size(),13);
+  Eigen::MatrixXd m = Eigen::MatrixXd::Zero(12*A_.size(),12);
+  Eigen::VectorXd b(12*A_.size());
   for(int i=0;i<A_.size();i++)
   {
     //extract R,t from homogophy matrix
@@ -22,31 +23,14 @@ Pose ConventionalAXXBSVDSolver::SolveX()
     Eigen::Matrix3d Ta_skew = skew(Ta);
     m.block<3,9>(12*i+9,0) = Eigen::kroneckerProduct(Eigen::MatrixXd::Identity(3,3),Tb.transpose());
     m.block<3,3>(12*i+9,9) = Eigen::MatrixXd::Identity(3,3) - Ra;
-    m.block<3,1>(12*i+9,12) = -Ta;
+    b.block<3,1>(12*i+9,0) = -Ta;
   }
 
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd( m, Eigen::ComputeFullV | Eigen::ComputeFullU );
-  CHECK(svd.computeV())<<"fail to compute V";
+  Eigen::Matrix<double, 12, 1> x = m.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
+  Eigen::Matrix3d R = Eigen::Map<Eigen::Matrix3d>(x.data());
 
-  Eigen::Matrix3d R_alpha;
-  R_alpha.row(0) = svd.matrixV().block<3,1>(0,12).transpose();
-  R_alpha.row(1) = svd.matrixV().block<3,1>(3,12).transpose();
-  R_alpha.row(2) = svd.matrixV().block<3,1>(6,12).transpose();
-  //double a = std::fabs(R_alpha.determinant());
-  double det = R_alpha.determinant();
-  double alpha = std::pow(std::abs(det),4./3.)/det;
-
-  Eigen::HouseholderQR<Eigen::Matrix3d> qr(R_alpha/alpha);
-
-  Pose handeyetransformation = Pose::Identity(4,4);
-  Eigen::Matrix3d Q = qr.householderQ();
-  Eigen::Matrix3d Rwithscale = alpha*Q.transpose()*R_alpha;
-  Eigen::Vector3d R_diagonal = Rwithscale.diagonal();
-  for(int i=0;i<3;i++)
-  {
-    handeyetransformation.block<3,1>(0,i) = int(R_diagonal(i)>=0?1:-1)*Q.col(i);
-  }
-
-  handeyetransformation.topRightCorner(3,1) = svd.matrixV().block<3,1>(9,12)/alpha;
+  Eigen::JacobiSVD<Eigen::Matrix3d> svd(R, Eigen::ComputeThinU | Eigen::ComputeThinV);
+  handeyetransformation.topLeftCorner(3,3) = svd.matrixU() * svd.matrixV().transpose();
+  handeyetransformation.topRightCorner(3,1) = x.block<3,1>(9,12);
   return handeyetransformation;
 }
